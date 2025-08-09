4e8be41f5b633d84debb0f6c96d1ea79f0abed29
Vivek Jha <progamervivek2020@gmail.com>
2025-08-09 12:22:57 +0530

    .

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}: New Req recived\n`;
-  fs.appendFile("log.txt", log, (err, data) => {
-    res.end("Hello, World!\n");
+  if (req.url === "/favicon.ico") {
+    return res.end();
+  }
+
+  const log = `${Date.now()}: ${req.url} - New request received\n`;
+  const myUrl = url.parse(req.url, true); // fixed variable name
+  console.log(myUrl);
+
+  fs.appendFile("log.txt", log, (err) => {
+    if (err) {
+      console.error("Error writing to log file:", err);
+      res.statusCode = 500;
+      return res.end("Internal Server Error");
+    }
+
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("<h1>Welcome to the Home Page</h1>");
+        break;
+
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`<h1>Welcome, ${username}, to the Bro Page</h1>`);
+        break;
+
+      default:
+        res.statusCode = 404;
+        res.end("<h1>404 Not Found</h1>");
+        break;
+    }
   });
 });
 
-myServer.listen(8000, () => console.log("Server is running on port 3000"));
+myServer.listen(8000, () => console.log("Server is running on port 8000"));
