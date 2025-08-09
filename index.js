42943523344f6b95844d442f94277ad2d6ab3682
Vivek Jha <progamervivek2020@gmail.com>
2025-08-09 11:48:23 +0530

    .

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-  console.log("hi");
-  res.end("Hello, World!\n");
+  const log = `${Date.now()}: New Req recived\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    res.end("Hello, World!\n");
+  });
 });
 
-myServer.listen(3000, () => console.log("Server is running on port 3000"));
+myServer.listen(8000, () => console.log("Server is running on port 3000"));
